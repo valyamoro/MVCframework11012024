22bbc4e02f435297eb183639065f91ab539a7e5b
kutlumbek <Moralek228@gmail.com>
2024-01-11 14:41:19 +0300

    Добавил метод рендеринга в контроллер

--- core/View.php
+++ core/View.php
@@ -6,8 +6,6 @@ class View
 {
     public static function render(string $view, array $params)
     {
-        \extract($params);
-        \ob_start();
-        include __DIR__ . "/../Views/{$view}.php";
+        
     }
 }

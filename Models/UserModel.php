6de7c006492006537d6a165fd321d96ef14939bd
kutlumbek <Moralek228@gmail.com>
2024-01-11 17:45:30 +0300

    Добавил регистрацию, пока без валидации

--- Models/UserModel.php
+++ Models/UserModel.php
@@ -6,5 +6,11 @@ use app\core\Model;
 
 class UserModel extends Model
 {
+    public string $id;
+    public string $firstName;
+    public string $lastName;
+    public string $email;
+    public string $password;
 
+    protected const TABLE_NAME = 'users';
 }
